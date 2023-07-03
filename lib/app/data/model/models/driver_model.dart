da9323452fc40756b791fd71352698c8b0f4277d
hossienhunta <hossien021.m@yahoo.com>
2023-07-04 00:52:08 +0330

    feat: Add DriverController for managing driver information in ObjectBox database
    This commit adds the DriverController class for managing driver information in an ObjectBox database for a Flutter application. The controller inherits from GetXController and utilizes the state management and reactivity features of the GetX library. The controller includes several different fields and methods for managing and creating driver information, as well as Rx variables for managing the state of the application. This controller provides a robust and efficient way to manage driver information in a Flutter application using the ObjectBox database.

--- lib/app/data/model/models/driver_model.dart
+++ lib/app/data/model/models/driver_model.dart
@@ -4,34 +4,29 @@ import 'package:objectbox/objectbox.dart';
 class DriverModel {
   @Id()
   int id = 0;
-  final String driverName;
-  final String? driverCreditCard;
-  final String? amount;
-  @Property(type: PropertyType.date)
-  final DateTime date;
+  String driverName;
+  String? amount;
+  String date;
 
   DriverModel({
     required this.driverName,
-    this.driverCreditCard,
     this.amount,
     required this.date,
   });
 
   DriverModel.fromJson(Map<String, dynamic> json)
       : driverName = json['driverName'],
-        driverCreditCard = json['driverCreditCard'],
         amount = json['amount'],
         date = json['date'];
 
   Map<String, dynamic> toJson() => {
         'driverName': driverName,
-        'driverCreditCard': driverCreditCard,
         'amount': amount,
         'date': date,
       };
 
   @override
   String toString() {
-    return 'Item{id: $id, name: $driverName, creditCardNumber: $driverCreditCard, creditCardShaba: $amount, date: $date}';
+    return 'Item{id: $id, name: $driverName, creditCardShaba: $amount, date: $date}';
   }
 }
--- lib/app/modules/drivers/driver_controller.dart
+++ lib/app/modules/drivers/driver_controller.dart
@@ -3,37 +3,28 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:karpardaz/app/data/model/repository.dart';
 import 'package:karpardaz/app/data/services/repository.dart';
-import 'package:karpardaz/core/utils/utils.dart';
 import 'package:karpardaz/objectbox.g.dart';
 
 class DriverController extends GetxController {
-  late TextEditingController textDriverCreditCardsNumberController,
-      textDriverNameController,
+  late TextEditingController textDriverNameController,
       textAmountController,
       textDateController,
       textUpdateDriverNameController,
-      textUpdateDriverCreditCardsNumberController,
       textUpdateAmountController,
       textUpdateDateController;
   late Box<DriverModel> _driverBox;
   RxList<DriverModel> itemList = <DriverModel>[].obs;
   final ObjectBoxService _objectBoxService = Get.find();
   final DriverBackupService driverBackupService = Get.find();
-  final RxBool _creditCardsFieldError = false.obs;
   final RxBool _amountFieldError = false.obs;
-  final RxBool _dateFieldError = false.obs;
   final RxBool _nameFieldError = false.obs;
   final RxBool _isLoadingVertical = false.obs;
+  final RxString e = ''.obs;
   final textDriverCreditCardsNumber = ''.obs;
   final textDriverName = ''.obs;
   final textAmount = ''.obs;
-  final textDate = DateTime.now();
-
-  bool get creditCardsFieldError => _creditCardsFieldError.value;
 
   bool get amountFieldError => _amountFieldError.value;
-
-  bool get dateFieldError => _dateFieldError.value;
 
   bool get nameFieldError => _nameFieldError.value;
 
@@ -43,11 +34,9 @@ class DriverController extends GetxController {
   void onInit() async {
     super.onInit();
     textDriverNameController = TextEditingController();
-    textDriverCreditCardsNumberController = TextEditingController();
     textAmountController = TextEditingController();
     textDateController = TextEditingController();
     textUpdateDriverNameController = TextEditingController();
-    textUpdateDriverCreditCardsNumberController = TextEditingController();
     textUpdateAmountController = TextEditingController();
     textUpdateDateController = TextEditingController();
     _driverBox = _objectBoxService.driverBox;
@@ -56,12 +45,10 @@ class DriverController extends GetxController {
 
   @override
   void onClose() {
-    textDriverCreditCardsNumberController.dispose();
     textDriverNameController.dispose();
     textAmountController.dispose();
     textDateController.dispose();
     textUpdateDriverNameController.dispose();
-    textUpdateDriverCreditCardsNumberController.dispose();
     textUpdateAmountController.dispose();
     textUpdateDateController.dispose();
     super.onClose();
@@ -71,35 +58,29 @@ class DriverController extends GetxController {
     _isLoadingVertical.value = true;
   }
 
-  void onBackup() {
-    driverBackupService.driverBackup(_driverBox.getAll());
-  }
-
-  void onRestore() async {
-    final list = await driverBackupService.driverRestore();
-    if (list == [] || list.isEmpty) {
-      onBackup();
-    } else {
-      _driverBox.removeAll();
-      _driverBox.putMany(list);
-      onUpdate();
+  bool onBackup() {
+    try {
+      driverBackupService.driverBackup(_driverBox.getAll());
+      return true;
+    } catch (E) {
+      e.value = E.toString();
+      return false;
     }
   }
 
-  bool validateTextCreditCardsNumber(String value) {
-    if (value.length == 16) {
-      if (testCreditCardBank(value) && testCreditCardNumber(value)) {
+  Future<bool> onRestore() async {
+    try {
+      final list = await driverBackupService.driverRestore();
+      if (list == [] || list.isEmpty) {
+        onBackup();
+      } else {
+        _driverBox.removeAll();
+        _driverBox.putMany(list);
+        onUpdate();
         return true;
       }
-    }
-    return false;
-  }
-
-  bool validateTextCreditCardsIBAN(String value) {
-    if (value.length == 26) {
-      if (testCreditCardIBANBank(value) && testCreditCardIBAN(value)) {
-        return true;
-      }
+    } catch (E) {
+      e.value = E.toString();
     }
     return false;
   }
@@ -116,80 +97,52 @@ class DriverController extends GetxController {
   }
 
   void btnSubmit() async {
-    // var iban = fixIBAN(textAmountController.text);
-    // if (validateTextCreditCardsNumber(
-    //     textDriverCreditCardsNumberController.text)) {
-    //   if (validateTextCreditCardsIBAN(iban)) {
-    //     iban = fixIBAN('IR');
-    //     if (textDriverNameController.text.length >= 3) {
-    //       if (validateTextCreditCardsNumber(
-    //               textDriverCreditCardsNumberController.text) ||
-    //           validateTextCreditCardsIBAN(iban)) {
-    //         _creditCardsFieldError.value = false;
-    //         _amountFieldError.value = false;
-    //         _nameFieldError.value = false;
-    //         final item = DriverModel(
-    //           driverName: textDriverNameController.text,
-    //           driverCreditCard: textDriverCreditCardsNumberController.text,
-    //           amount: iban,
-    //           // date: textUpdateAmountController.text
-    //         );
-    //         _driverBox.put(item);
-    //         itemList.add(item);
-    //         onUpdate();
-    //         textDriverNameController.clear();
-    //         textDriverCreditCardsNumberController.clear();
-    //         textAmountController.clear();
-    //       } else {
-    //         _creditCardsFieldError.value = true;
-    //         _amountFieldError.value = true;
-    //       }
-    //     } else {
-    //       _nameFieldError.value = true;
-    //     }
-    //   } else {
-    //     _amountFieldError.value = true;
-    //   }
-    // } else {
-    //   _creditCardsFieldError.value = true;
-    // }
+    final alphanumeric = RegExp(r'^\d{1,3}(,\d{3})*(\.\d+)?$');
+    final result = alphanumeric.hasMatch(textAmountController.text);
+    if (result) {
+      _amountFieldError.value = false;
+      if (textDriverNameController.text.length >= 3) {
+        _nameFieldError.value = false;
+        print(textDateController.text);
+        final item = DriverModel(
+          driverName: textDriverNameController.text,
+          date: textDateController.text,
+          amount: textAmountController.text,
+        );
+        _driverBox.put(item);
+        itemList.add(item);
+        onUpdate();
+        textDriverNameController.clear();
+        textAmountController.clear();
+      } else {
+        _nameFieldError.value = true;
+      }
+    } else {
+      _amountFieldError.value = true;
+    }
   }
 
   void btnUpdate(int id) {
-    //   final index = itemList.indexWhere((element) => element.id == id);
-    //   var iban = fixIBAN(textUpdateCreditCardsIBANController.text);
-    //   if (validateTextCreditCardsNumber(textDateController.text)) {
-    //     if (validateTextCreditCardsIBAN(iban)) {
-    //       iban = fixIBAN('IR');
-    //       if (textUpdateCreditCardsNameController.text.length >= 3) {
-    //         if (validateTextCreditCardsNumber(textDateController.text) ||
-    //             validateTextCreditCardsIBAN(iban)) {
-    //           _creditCardsFieldError.value = false;
-    //           _ibanFieldError.value = false;
-    //           _nameFieldError.value = false;
-    //           itemList[index].creditCardName =
-    //               textUpdateCreditCardsNameController.text;
-    //           itemList[index].creditCardNumber = textDateController.text;
-    //           itemList[index].creditCardIBAN = iban;
-    //           _creditCardBox.put(itemList[index]);
-    //           Get.back();
-    //           onUpdate();
-    //           textUpdateCreditCardsNameController.clear();
-    //           textDateController.clear();
-    //           textUpdateCreditCardsIBANController.clear();
-    //         } else {
-    //           _creditCardsFieldError.value = true;
-    //           _ibanFieldError.value = true;
-    //         }
-    //       } else {
-    //         _nameFieldError.value = true;
-    //       }
-    //     } else {
-    //       _ibanFieldError.value = true;
-    //     }
-    //   } else {
-    //     _creditCardsFieldError.value = true;
-    //   }
-    // }
+    final index = itemList.indexWhere((element) => element.id == id);
+    final alphanumeric = RegExp(r'^\d{1,3}(,\d{3})*(\.\d+)?$');
+    final result = alphanumeric.hasMatch(textUpdateAmountController.text);
+    if (result) {
+      _amountFieldError.value = false;
+      if (textUpdateDriverNameController.text.length >= 3) {
+        _nameFieldError.value = false;
+        itemList[index].driverName = textUpdateDriverNameController.text;
+        itemList[index].amount = textUpdateAmountController.text;
+        itemList[index].date = textUpdateDateController.text;
+        _driverBox.put(itemList[index]);
+        Get.back();
+        onUpdate();
+        textUpdateDriverNameController.clear();
+        textUpdateAmountController.clear();
+      } else {
+        _nameFieldError.value = true;
+      }
+    } else {
+      _amountFieldError.value = true;
+    }
   }
 }
